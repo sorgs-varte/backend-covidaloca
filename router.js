82cca77b97025f6fa2b3dc0a45eb20849896934a
anthony <anthony@sadighconseil.com>
2020-04-06 16:05:47 +0200

    add cors

--- router.js
+++ router.js
@@ -1,44 +1,40 @@
 const passport = require('passport');
 require('./Services/passport');
+const cors = require('cors');
 const requireAuth = passport.authenticate('jwt', { session : false });
 const requireSignin = passport.authenticate('local', { session: false });
 const users = require('./app/controllers/user.controller.js');
+const bodyParser = require('body-parser');
 const categories = require('./app/controllers/category.controller.js');
 const activities = require('./app/controllers/activity.controller.js');
 
 module.exports = (app) => {
 
+
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:443');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        next();
+    });
+    app.use(cors());
+    app.use(bodyParser({limit: '500mb'}));
+    app.use(bodyParser.json({type: '*/*'}));
+    app.use(bodyParser.json({limit: '500mb'}));
+    app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
+
     // Create a new User
     app.post('/signup', users.signup);
 
-    // Retrieve all Users
-    /*app.get('/users',requireAuth, users.findAll);
-
-    // Retrieve a single User with userId
-    app.get('/users/:userId',requireAuth, users.findOne);
-
-    // Update a User with userId
-    app.put('/users/:nuserId',requireAuth, users.update);
-
-    // Delete a User with userId
-    app.delete('/users/:userId',requireAuth, users.delete);*/
-
+    app.post('/login', users.login);
 
     // Create a new category
     app.post('/categories',requireAuth, categories.create);
 
     // Retrieve all categories
     app.get('/categories',requireAuth, categories.findAll);
-
-    // Retrieve a single category with categoryId
-    app.get('/categories/:categoryId',requireAuth, categories.findOne);
-
-    // Update a category with categoryId
-    app.put('/categories/:categoryId',requireAuth, categories.update);
-
-    // Delete a category with categoryId
-    app.delete('/categories/:categoryId',requireAuth, categories.delete);
-
 
 
     // Create a new activity
